20b6de1b1b5829bcc0b0861a53684e7df54fda83
Dodik-Dot <dodik.kurniawan@borwita.co.id>
2026-09-23 16:37:59 +0700

    Update install.ps1

--- windows/install.ps1
+++ windows/install.ps1
@@ -13,7 +13,7 @@ if (-not $isAdmin) {
 # 2. Konfigurasi Default & Parser Argumen Manual
 $ServerIP      = "192.168.55.112"       # Default IP Server Checkmk
 $SiteName      = "cmk"                  # Default Site ID Checkmk Anda
-$AgentVersion  = "2.4.0p35"           # Default Versi Agen Checkmkmk-agent_2.4.0p27
+$AgentVersion  = "2.5.0p14"             # Default Versi Agen Checkmkmk-agent_2.4.0p27
 $GithubUser    = "Dodik-Dot"            # Username GitHub Anda
 $GithubRepo    = "configcmk"            # Nama repositori GitHub Anda
 $Branch        = "main"
