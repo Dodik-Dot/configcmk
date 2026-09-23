c95e9955480e07a825e69fd21dc8b7c68ad10ded
Dodik-Dot <dodik.kurniawan@borwita.co.id>
2026-09-23 09:39:22 +0700

    Update install.ps1

--- windows/install.ps1
+++ windows/install.ps1
@@ -10,12 +10,12 @@ if (-not $isAdmin) {
     Exit
 }
 
-# 2. Konfigurasi Default & Parser Argumen Manual (Menghindari error 'param' di IEX)
-$ServerIP      = "192.168.1.100"       # Default IP Server Checkmk
-$SiteName      = "cmk"                 # Default Site ID Checkmk Anda
-$AgentVersion  = "2.5.0p9"             # Default Versi Agen Checkmk
-$GithubUser    = "andin1st"            # Username GitHub Anda
-$GithubRepo    = "scriptcmk"           # Nama repositori GitHub Anda
+# 2. Konfigurasi Default & Parser Argumen Manual
+$ServerIP      = "192.168.55.112"       # Default IP Server Checkmk
+$SiteName      = "cmk"                  # Default Site ID Checkmk Anda
+$AgentVersion  = "2.4.0p35-1"           # Default Versi Agen Checkmk
+$GithubUser    = "Dodik-Dot"            # Username GitHub Anda
+$GithubRepo    = "configcmk"            # Nama repositori GitHub Anda
 $Branch        = "main"
 
 # Parsing argumen manual dari $args
